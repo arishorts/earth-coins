66ea50062744183e5e871054a0abf08ecbe885a9
Joseph <117396146+joeytraffic@users.noreply.github.com>
2023-06-08 20:29:20 -0700

    made links open in new tab

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,16 +9,20 @@ const Footer = () => {
           Powered by{" "}
           <a
             href="https://www.coingecko.com/"
+            target="_blank"
             className="font-bold transition duration-300 hover:text-sky-500"
           >
             CoinGecko
           </a>
         </p>
-        <img
-          src="https://static.coingecko.com/s/thumbnail-d5a7c1de76b4bc1332e48227dc1d1582c2c92721b5552aae76664eecb68345c9.png"
-          alt="CoinGecko"
-          className="image-text-size"
-        />
+
+        <a href="https://www.coingecko.com/" target="_blank">
+          <img
+            src="https://static.coingecko.com/s/thumbnail-d5a7c1de76b4bc1332e48227dc1d1582c2c92721b5552aae76664eecb68345c9.png"
+            alt="CoinGecko"
+            className="image-text-size"
+          />
+        </a>
       </Container>
     </footer>
   );
